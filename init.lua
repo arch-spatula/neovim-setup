6ba6082b03cadbd7f9ab93256f0633fbb4de4f04
arch-spatula <shkim516@naver.com>
2024-01-11 08:24:15 +0900

    neovim 설정 보존

---
+++ init.lua
@@ -0,0 +1,368 @@
+print("hello soydevs")
+print("C언어나 하자")
+
+print("⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠋⠉⠉⠉⠙⠛⢿⠿⠛⠛⠛⠛⠛⠿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⢀⡀⠤⠤⢄⣀⠐⡄⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣿⣿⠟⠀⠀⠀⠐⠁⠀⠀⢀⣀⣀⣉⣒⣄⠉⠉⠉⢉⣀⣂⠈⠙⠻⣿⣿")
+print("⣿⣿⣿⠟⠉⡄⠀⠀⠀⠠⠤⣐⠭⠒⠉⠉⠉⠉⣒⣳⠈⠛⠂⠀⠀⣒⣛⡢⠈⢿")
+print("⣿⣿⠁⠀⠀⠃⠀⠀⠈⠛⠛⠠⣤⣶⠖⠙⠀⡉⢻⡿⣶⣿⣿⠁⡂⠠⠈⢿⠗⢸")
+print("⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠂⠭⠤⠤⠤⣤⠄⠊⠀⠀⠈⠀⠀⠀⠀⢀⣶⣿")
+print("⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠉⠀⠀⠀⠈⠁⠒⠂⠈⠁⠘⢿⣿⣿")
+print("⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿")
+print("⡇⠀⠀⠀⠀⠀⠀⠀⠀⣾⣟⣛⣛⣻⡶⠶⠶⣶⣤⣤⣤⣤⣤⣤⣤⡶⠾⢃⣼⣿")
+print("⣷⡀⠀⠀⠀⠀⠀⠘⢄⡀⠀⠀⠉⠉⠙⠛⠛⠛⠓⠛⠻⠿⠿⠷⠿⠟⢃⣼⣿⣿")
+print("⣿⣿⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿")
+print("⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⡿⠁⠀⠀⢠⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡶⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⠃⠀⠀⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⠀⢠⢶⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⡄⠀⢠⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠋⢸⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⡏⠀⠀⠀⠀⢰⣶⣤⣤⣤⣤⣤⣴⣦⡀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+print("⣿⣿⣿⣷⣄⣀⣀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣄⣀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+
+-- 줄번호 표시하게 만들기
+vim.wo.number = true
+-- 현재 줄기준으로 표시
+vim.wo.relativenumber = true
+
+-- 1 tab을 4 space로 변환
+vim.opt["tabstop"] = 2
+vim.opt["shiftwidth"] = 2
+
+-- 2칸
+vim.opt["numberwidth"] = 2
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- lazyvim 플러그인 설치
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 스페이스바 + e + x 으로 vim 탐색
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+require('lazy').setup({
+	-- 검색
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			{
+				'nvim-telescope/telescope-fzf-native.nvim',
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = 'make',
+				cond = function()
+					return vim.fn.executable 'make' == 1
+				end,
+			},
+		}
+	},
+
+	-- 태마 설정
+	{ "catppuccin/nvim",         name = "catppuccin", priority = 1000 },
+
+	-- 하단 상태바
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+
+	-- 사이드바
+	{
+		"nvim-tree/nvim-tree.lua",
+		cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {}
+		end,
+	},
+
+	{
+		-- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ':TSUpdate',
+	},
+
+	-- 들여쓰기 설정
+	{
+		'lukas-reineke/indent-blankline.nvim',
+		main = 'ibl',
+		opts = {},
+	},
+
+	-- lsp 설정
+	{
+		'neovim/nvim-lspconfig',
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{ 'williamboman/mason.nvim', config = true },
+			'williamboman/mason-lspconfig.nvim',
+
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ 'j-hui/fidget.nvim',       opts = {} },
+
+			-- Additional lua configuration, makes nvim stuff amazing!
+			'folke/neodev.nvim',
+		}
+	},
+	-- 자동완성
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-path',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+		},
+	},
+
+	{
+		-- git 설정
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map({ 'n', 'v' }, ']c', function()
+					if vim.wo.diff then
+						return ']c'
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return '<Ignore>'
+				end, { expr = true, desc = 'Jump to next hunk' })
+
+				map({ 'n', 'v' }, '[c', function()
+					if vim.wo.diff then
+						return '[c'
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return '<Ignore>'
+				end, { expr = true, desc = 'Jump to previous hunk' })
+
+				-- Actions
+				-- visual mode
+				map('v', '<leader>hs', function()
+					gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+				end, { desc = 'stage git hunk' })
+				map('v', '<leader>hr', function()
+					gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+				end, { desc = 'reset git hunk' })
+				-- normal mode
+				map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
+				map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
+				map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
+				map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
+				map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
+				map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
+				map('n', '<leader>hb', function()
+					gs.blame_line { full = false }
+				end, { desc = 'git blame line' })
+				map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
+				map('n', '<leader>hD', function()
+					gs.diffthis '~'
+				end, { desc = 'git diff against last commit' })
+
+				-- Toggles
+				map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
+				map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+
+				-- Text object
+				map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
+			end,
+		},
+	},
+	-- 저장에 자동 포맷팅
+	require 'plugins.autoformat',
+	-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+
+	-- 주석처리하는 방법
+	{
+		'numToStr/Comment.nvim',
+		opts = {
+			-- add any options here
+			padding = true,
+			-- sticky = true,
+			toggler = {
+				---Line-comment toggle keymap
+				line = "gcc",
+				---Block-comment toggle keymap
+				block = "gbc",
+			},
+			opleader = {
+				---Line-comment keymap
+				-- line = "<leader>/",
+				---Block-comment keymap
+				-- block = "<leader>/",
+			},
+			mappings = {
+				---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+				basic = true,
+				---Extra mapping; `gco`, `gcO`, `gcA`
+				extra = true,
+			},
+		},
+		lazy = false,
+	},
+	{ 'akinsho/bufferline.nvim', version = "*",       dependencies = 'nvim-tree/nvim-web-devicons' }
+})
+
+-- 검색기 활성화
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+-- catppuccin 컬러 태마 설정
+vim.cmd.colorscheme "catppuccin-mocha"
+
+-- 클립보드 공유
+vim.o.clipboard = 'unnamedplus'
+
+-- lualine.nvim 설정
+require('lualine').setup()
+
+-- 사이드 펴기 단축키
+vim.keymap.set('n', '<C-n>', vim.cmd.NvimTreeToggle, {})
+vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeFocus, {})
+
+-- syntax 하이라이팅
+vim.defer_fn(function()
+	require('nvim-treesitter.configs').setup {
+		-- Add languages to be installed here that you want installed for treesitter
+		ensure_installed = { 'c', 'cpp', 'lua', 'rust', 'zig', 'vimdoc', 'vim', 'bash' },
+		highlight = { enable = true },
+		indent = { enable = true }
+	}
+end, 0
+)
+
+-- 자동완성 설정
+require('mason').setup()
+require("mason-lspconfig").setup {
+	ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
+}
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").clangd.setup {
+	on_attach = function(client, bufnr)
+		client.server_capabilities.signatureHelpProvider = false
+		on_attach(client, bufnr)
+	end,
+}
+
+-- C언어 자동완성???
+local cmp = require 'cmp'
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' }, -- For vsnip users.
+		{ name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['lua_ls'].setup {
+	capabilities = capabilities
+}
+require('lspconfig')['clangd'].setup {
+	capabilities = capabilities
+}
+require('lspconfig')['marksman'].setup {
+	capabilities = capabilities
+}
+
+-- leader + / 에 주석 처리 -> 아직 완성 못함 gcc, gbc로 동작함
+-- vim.keymap.set('n', '<leader>/', "gcc") -- 동작안함
+-- vim.keymap.set('v', '<leader>/', "gbc") -- 동작안함
+-- vim.keymap.set('n', '<leader>d', "dd") -- 정상동작
+
+-- buffer 표시
+-- 디버거 추가
+-- 마크다운 코드블럭 syntax 하이라이트
+-- 탤레스코프 더 구체적이게 nvchad 기준으로 설정하기
+-- 화면 분할(커맨드라인 & 다른 버퍼)
+-- 버퍼 탭
+-- leader 이후 접근 할 수 있는 커맨드 표시(다른 distro 참고)
+-- 레포에 설정 저장(git commit)
