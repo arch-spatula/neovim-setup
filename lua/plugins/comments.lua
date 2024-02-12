5df11e78e4fc67d51916c60db861e3df81272ed8
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-02-12 10:16:51 +0900

    refactor: lsp와 무관한 플러그인 모듈별 이동 #3

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,24 +1,71 @@
-local M = {}
+--local M = {}
 
-M.comment = {
-	plugin = true,
+--M.comment = {
+--plugin = true,
 
-	-- toggle comment in both modes
-	n = {
-		["<leader>/"] = {
-			function()
-				require("Comment.api").toggle.linewise.current()
-			end,
-			"Toggle comment",
-		},
-	},
+---- toggle comment in both modes
+--n = {
+--["<leader>/"] = {
+--function()
+--require("Comment.api").toggle.linewise.current()
+--end,
+--"Toggle comment",
+--},
+--},
 
-	v = {
-		["<leader>/"] = {
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-			"Toggle comment",
-		},
-	},
+--v = {
+--["<leader>/"] = {
+--"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+--"Toggle comment",
+--},
+--},
+--}
+
+--return M
+
+-- 저장에 자동 포맷팅
+-- require 'plugins.autoformat',
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+
+---- 주석처리하는 방법
+--{
+--'numToStr/Comment.nvim',
+--opts = {
+---- add any options here
+--padding = true,
+---- sticky = true,
+--toggler = {
+-----Line-comment toggle keymap
+--line = "gcc",
+-----Block-comment toggle keymap
+--block = "gbc",
+--},
+--opleader = {
+-----Line-comment keymap
+--line = "<leader>_",
+-----Block-comment keymap
+--block = "<leader>_",
+--},
+--mappings = {
+-----Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+--basic = true,
+-----Extra mapping; `gco`, `gcO`, `gcA`
+--extra = true,
+--},
+--},
+--lazy = false,
+--},
+
+return {
+	{
+		"preservim/nerdcommenter",
+		config = function()
+			-- leader + / 에 주석 처리 -> 아직 완성 못함 지금은 leader + c 로 주석처리함
+			local modes = { 'n', 'v' }
+			for i in pairs(modes) do
+				vim.keymap.set(modes[i], '<leader>c', ':call nerdcommenter#Comment(0, "toggle")<CR>',
+					{ noremap = true, silent = true })
+			end
+		end
+	}
 }
-
-return M
---
+++ lua/plugins/indent.lua
@@ -0,0 +1,8 @@
+return {	
+	-- 들여쓰기 구분선 설정
+	{
+		'lukas-reineke/indent-blankline.nvim',
+		main = 'ibl',
+		opts = {},
+	},
+}
