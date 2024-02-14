e8c6003e3c0e9c19aa7c582490626672c715e38d
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-02-14 21:48:24 +0900

    feat: 하이라이트 설정 #4

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -1,2 +1,14 @@
 return {
+	-- :TSInstallInfo 로 설치 정보 확인
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+				-- 하이라이트할 언어 추가
+        ensure_installed = {"lua", "vim", "vimdoc", "c", "cpp", "rust", "html", "css", "javascript", "typescript", "zig"},
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
 }
