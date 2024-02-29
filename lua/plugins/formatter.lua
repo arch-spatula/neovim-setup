ef8581d43dfcbf4637926afc6d73923d6c471cbb
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-02-29 21:29:25 +0900

    chore: eslint_d 주석처리 #6

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,7 +5,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.eslint_d,
+				--null_ls.builtins.diagnostics.eslint_d,
+				--eslint_language_server으로 대체
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.completion.spell,
 			},
