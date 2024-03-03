546f51eba2a675215d74394cc1dca55d2a5ebca8
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-03 16:56:44 +0900

    Feat/telescope reconfig (#10)
    * chore: preview 보이게 하는 설정 알아냄 #5
    
    * chore: eslint_d 주석처리 #6
    
    * feat: code action 추가 #5
    
    * refactor: lsp 리팩토링 정리 #6
    
    * refactor: 설치를 먼저 실행하고 설정을 한번에 함 #5
    
    - preview 안보이는 이유 추가
    
    * fix: LSP 확성 설치 설정 #5

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,67 @@
 -- :Mason 명령으로 확인하지만 설정은 코드로 합니다.
 return {
 	{
-		'williamboman/mason.nvim',
+		"williamboman/mason.nvim",
 		config = function()
-			require("mason").setup()
-		end
+			require("mason").setup({})
+		end,
 	},
 	{
-		'williamboman/mason-lspconfig.nvim',
+		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			-- @todo "stylua" 같은 포맷터 ensure_installed 처리하기
-			require("mason").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "cssls", "eslint", "tsserver", "html", "volar" },
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"clangd",
+					"gopls",
+					"marksman",
+					-- JS & TS
+					"html",
+					"cssls",
+					"eslint",
+					"tsserver",
+					"volar",
+				},
+				--automatic_installation = true
 			})
-		end
+		end,
 	},
 	-- :LspInfo 로 LSP 설정 확인 가능
 	-- :h vim.lsp.buf 현재 버퍼의 도음말도 찾는 것이 가능합니다.
 	{
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			-- @todo lsp attach hook 적용
-			local lspconfig = require('lspconfig')
+			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.clangd.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
+			-- JS & TS
 			lspconfig.tsserver.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
+			})
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
 			})
 			lspconfig.volar.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
+			})
+
+			-- Go
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
 			})
 
 			-- @todo 키맵 추가
-			vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
-	}
+	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,62 @@
+--  '<leader>fh' telescope.nvim 검색으로 문서를 확인하기 바랍니다.
+--  preview는 너무 작으면 안보임 1920 * 1080 해상도에서 시도하면 보임 https://www.reddit.com/r/neovim/comments/nasx24/telescope_doesnt_show_preview/
+--  https://github.com/cpow/neovim-for-newbs/blob/main/lua/plugins/telescope.lua 을 참고함
 return {
-	'nvim-telescope/telescope.nvim',
-	branch = '0.1.x',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		{
-			'nvim-telescope/telescope-fzf-native.nvim',
-			-- NOTE: If you are having trouble with this installation,
-			--       refer to the README for telescope-fzf-native for more instructions.
-			build = 'make',
-			cond = function()
-				return vim.fn.executable 'make' == 1
-			end,
-		},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
 	},
-	init = function()
-		-- 검색기 활성화
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- 파일이름으로 검색
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) -- 단어 검색
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})  -- buffer 탭 검색
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-	end
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				-- 아래 주석 한번 걸어보면 preview ui가 생김
+				--build = "make",
+				--cond = function()
+				--return vim.fn.executable("make") == 1
+				--end,
+			},
+		},
+		config = function()
+			-- code action telescope 조합 설정
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+						}),
+					},
+				},
+			})
+			-- pseudo code / specification for writing custom displays, like the one
+			-- for "codeactions"
+			-- specific_opts = {
+			--   [kind] = {
+			--     make_indexed = function(items) -> indexed_items, width,
+			--     make_displayer = function(widths) -> displayer
+			--     make_display = function(displayer) -> function(e)
+			--     make_ordinal = function(e) -> string
+			--   },
+			--   -- for example to disable the custom builtin "codeactions" display
+			--      do the following
+			--   codeactions = false,
+			-- }
+
+			-- 검색기 활성화
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) -- 파일이름으로 검색
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- 단어 검색
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {}) -- buffer 탭 검색
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+
+			-- To get ui-select loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
