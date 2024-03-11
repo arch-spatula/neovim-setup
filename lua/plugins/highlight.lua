802beb93a14ca1b2b6c7c8b478f7f506f40c6266
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-11 21:09:39 +0900

    feat: treesitter/playground 설치 #17

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -1,17 +1,37 @@
 return {
-	-- :TSInstallInfo 로 설치 정보 확인
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter-textobjects'
+	{
+		-- :TSInstallInfo 로 설치 정보 확인
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				-- 하이라이트할 언어 추가
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"c",
+					"cpp",
+					"rust",
+					"html",
+					"css",
+					"javascript",
+					"typescript",
+					"zig",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
 	},
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			-- 하이라이트할 언어 추가
-			ensure_installed = { "lua", "vim", "vimdoc", "c", "cpp", "rust", "html", "css", "javascript", "typescript", "zig" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end
+	{
+		"nvim-treesitter/playground",
+		config = function()
+			vim.keymap.set("n", "<leader>tsp", vim.cmd.TSPlaygroundToggle, { desc = "[t]ree[s]itter [p]laygroundToggle" })
+		end,
+	},
 }
