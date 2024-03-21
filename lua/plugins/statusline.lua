b8a7486b09b4effcdfd03effd10b8b89b76b55e4
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-22 08:39:22 +0900

    feat: statusline 둥글둥글하게 만듬 (#46)

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,7 +1,23 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require('lualine').setup()
-	end
+		-- https://github.com/nvim-lualine/lualine.nvim/blob/master/examples/bubbles.lua
+		-- 위는 참고한 레포입니다.
+		require("lualine").setup({
+			options = { component_separators = "", section_separators = { left = "", right = "" } },
+			sections = {
+				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+				--lualine_b = { "filename", "branch" },
+				--lualine_c = {
+				--"%=", [> add your center compoentnts here in place of this comment <]
+				--},
+				--lualine_x = {},
+				--lualine_y = { "filetype", "progress" },
+				lualine_z = {
+					{ "location", separator = { right = "" }, left_padding = 2 },
+				},
+			},
+		})
+	end,
 }
