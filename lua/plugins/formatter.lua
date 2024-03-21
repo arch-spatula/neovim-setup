fa47ed99eb8654334031f805406fc8a6a1aae832
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-03-21 22:13:52 +0900

    feat: config as code를 위해 MasonInstallAll 명령 지원 (#44)

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,22 +1,24 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- 공통
-				null_ls.builtins.completion.spell,
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					-- 공통
+					null_ls.builtins.completion.spell,
 
-				--lua
-				null_ls.builtins.formatting.stylua,
+					--lua
+					null_ls.builtins.formatting.stylua,
 
-				-- JavaScript & TypeScript
-				--null_ls.builtins.diagnostics.eslint,
-				--eslint_language_server으로 대체
-				null_ls.builtins.formatting.prettierd,
-			},
-		})
+					-- JavaScript & TypeScript
+					--null_ls.builtins.diagnostics.eslint,
+					--eslint_language_server으로 대체
+					null_ls.builtins.formatting.prettierd,
+				},
+			})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "[g]o [f]ormat" })
-	end,
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "[g]o [f]ormat" })
+		end,
+	},
 }
