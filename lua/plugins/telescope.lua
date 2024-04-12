e8a9a5778306517912b834be5a8ac42b490c819e
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-04-13 00:12:18 +0900

    feat: telescope 키맵 추가 #35

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,7 +33,7 @@ return {
 				},
 			})
 			-- TODO: codeactions 설정하기
-			-- TODO: nvchad mapping과 kickstart mapping 적용하기
+			-- TODO: nvchad mapping 적용하기
 
 			-- pseudo code / specification for writing custom displays, like the one
 			-- for "codeactions"
@@ -51,11 +51,42 @@ return {
 
 			-- 검색기 활성화
 			local builtin = require("telescope.builtin")
+			-- NOTE: kickstart.nvim을 활용함
+			-- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]ile" }) -- 파일이름으로 검색
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[f]ind [g]rep" }) -- 단어 검색
+			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[f]ind current [w]ord" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[f]ind [b]uffers" }) -- buffer 탭 검색
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[f]ind [h]elp" })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[f]ind [k]eymaps" })
+			vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[f]ind [s]elect telescope" })
+			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[f]ind [r]esume" })
+			vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[f]ind recent files ("." for repeat)' })
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[f]ind [d]iagnostics" })
 			vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "[f]ind [g]it file" })
+
+			-- Slightly advanced example of overriding default behavior and theme
+			vim.keymap.set("n", "<leader>/", function()
+				-- You can pass additional configuration to telescope to change theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = " [/] Fuzzily search in current buffer" })
+
+			-- Also possible to pass additional configuration options.
+			--  See `:help telescope.builtin.live_grep()` for information about particular keys
+			vim.keymap.set("n", "<leader>f/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[f]ind [/] in Open Files" })
+
+			-- Shortcut for searching your neovim configuration files
+			vim.keymap.set("n", "<leader>fn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[f]ind [n]eovim files" })
 
 			-- To get ui-select loaded and working with telescope, you need to call
 			-- load_extension, somewhere after setup function:
