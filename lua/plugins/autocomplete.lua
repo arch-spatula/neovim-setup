d07dc574aa44129ee654f78893607d0aedf4bf88
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-09-17 21:36:27 +0900

    feat: 태그 닫기에 자동완성 추가 #48

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,28 +1,27 @@
 return {
 	-- NOTE: cmp-path,
-
 	{
-		"hrsh7th/cmp-nvim-lsp"
+		"hrsh7th/cmp-nvim-lsp",
 	},
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
-			'saadparwaiz1/cmp_luasnip',
-			'rafamadriz/friendly-snippets'
-		}
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			-- Set up nvim-cmp.
-			local cmp = require 'cmp'
+			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 					end,
 				},
 				window = {
@@ -30,19 +29,32 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' }, -- For luasnip users.
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
 				}, {
-					{ name = 'buffer' },
-				})
+					{ name = "buffer" },
+				}),
 			})
-		end
-	}
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"vue",
+			"javascript",
+			"typescript",
+			"jsx",
+			"tsx",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 }
